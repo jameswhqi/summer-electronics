06da6feab497493d929610ba8e44301cfb663a53
JamesQi <jameswhqi@gmail.com>
2017-08-30 17:06:55 +0800

    complete Core, add Core testbench.v

--- fpga/modules/Top.v
+++ fpga/modules/Top.v
@@ -1,14 +1,17 @@
 // Top module
 module Top
 (
+    // global reset
+    input rst,
     // crystal clock of 50MHz
     input clk,
     // binary signals of 4 infrared sensors
     input [3:0] ir,
     // binary signal of hall sensor
     input hall,
-    // square wave from color sensor
-    input color
+    // square wave from color sensors
+    input object_color
+    input station_color
     // PWM signal to servomotor
     output servo
     // IN pin of the 2 motor driver chips
@@ -22,30 +25,8 @@ module Top
     output buzz
 );
 
-    wire clk1;//10us
-    clkdiv #(9, 500) Clk1 (.clk(clk), .nclk(clk1));
-
-    wire clk2;//4ms
-    clkdiv #(9, 400) Clk2 (.clk(clk1), .nclk(clk2));
-
-    wire clk3;//0.5s
-    clkdiv #(7, 128) Clk3 (.clk(clk2), .nclk(clk3));
-
-    wire endiv;
-    wire [13:0] cnt;
-    count Count (.clk(clk), .clk1(clk1), .sig(sig), .endiv(endiv), .cntout(cnt));
-
-    wire [8:0] freq;
-    wire enbcd;
-    divide Divide (.clk(clk), .divider(cnt), .enable(endiv), .freq(freq), .enbcd(enbcd));
-
-    wire [11:0] digs;
-    reg [11:0] digs2;
-    bcd Bcd (.clk(clk), .freq(freq), .enable(enbcd), .digs(digs));
-
-    display Display (.clk2(clk2), .digs(digs2), .bit(bit), .seg(seg));
-
-    always @(posedge clk3)
-        digs2 <= digs;
+    Core core (.rst(rst), .clk(clk), .hall(hall), .object_color(object_color), .station_color(station_color),
+               .end_of_track(end_of_track), .uturn_finished(uturn_finished), .buzz_finished(buzz_finished),
+               .en_tracking(en_tracking), .en_uturn(en_uturn), .ssd_code(ssd_code), .en_buzz(en_buzz));
 
 endmodule
--- fpga/modules/core.v
+++ fpga/modules/core.v
@@ -1,28 +1,161 @@
 // Core module
-module Core
-(
+module Core (
+    // global reset
+    input rst,
+    // crystal clock of 50MHz
+    input clk,
     // binary signal from hall sensor
     input hall,
     // encoded color type from Color module
-    input [1:0] color,
+    input [1:0] object_color,
+    input [1:0] station_color,
     // feedback from Tracking module
     // end of track
     input end_of_track,
     // feedback from Uturn module
     // u-turn finished
     input uturn_finished,
+    // feedback from Buzzer module
+    // error buzzing finished
+    input buzz_finished,
     // enable Tracking module
-    output en_tracking,
+    output reg en_tracking,
     // enable Uturn module
-    output en_uturn,
+    output reg en_uturn,
     // status code to ssd control
     // 0 ready          1/2/3 sending red/green/blue    4/5/6 red/green/blue arrived
     // 7 end of track   8 u-turning                     9 returning
-    output [3:0] ssd_code,
-    // enable buzzer
-    output buzz
+    output reg [3:0] ssd_code,
+    // enable Buzzer module
+    output reg en_buzz
 );
 
-    
+    // fsm states
+    parameter   READY   = 7'b0000001,
+                NOCOLOR = 7'b0000010,
+                SEND    = 7'b0000100,
+                MATCH   = 7'b0001000,
+                UTURN   = 7'b0010000,
+                RETURN  = 7'b0100000,
+                EOT     = 7'b1000000;
+
+    // current state, next state
+    reg [6:0] cstate, nstate;
+
+    reg [1:0] object_color_detected;
+    // determines what to do after a u-turn
+    reg returning;
+
+    // change state
+    always @(posedge clk or negedge rst)
+        if (!rst)
+            cstate <= READY;
+        else
+            cstate <= nstate;
+
+    // state changing rules
+    always @(cstate)
+        case (cstate)
+            READY:
+                if (hall) begin
+                    if (in_color == 0)
+                        nstate = NOCOLOR;
+                    else
+                        nstate = SEND;
+                else
+                    nstate = READY;
+            NOCOLOR:
+                if (buzz_finished)
+                    nstate = READY;
+                else
+                    nstate = NOCOLOR;
+            SEND:
+                if (station_color == object_color_detected)
+                    nstate = MATCH;
+                else if (end_of_track)
+                    nstate = EOT;
+                else
+                    nstate = SEND;
+            MATCH:
+                if (hall)
+                    nstate = UTURN;
+                else
+                    nstate = MATCH;
+            UTURN:
+                if (uturn_finished)
+                    if (returning)
+                        nstate = READY;
+                    else
+                        nstate = RETURN;
+                else
+                    nstate = UTURN;
+            RETURN:
+                if (end_of_track)
+                    nstate = UTURN;
+                else
+                    nstate = RETURN;
+            EOT:
+                if (buzz_finished)
+                    nstate = UTURN;
+                else
+                    nstate = EOT;
+            default:
+                nstate = READY;
+        endcase
+
+    // en_tracking, en_uturn, ssd_code, en_buzz, object_color_detected, returning
+    always @(posedge clk or negedge rst)
+        if (rst) begin
+            en_tracking <= 0;
+            en_uturn <= 0;
+            ssd_code <= 0;
+            en_buzz <= 0;
+            object_color_detected <= 0;
+            returning <= 0;
+        end
+        else
+            case (nstate)
+                READY: begin
+                    en_uturn <= 0;
+                    ssd_code <= 0;
+                    en_buzz <= 0;
+                end
+                NOCOLOR:
+                    en_buzz <= 1;
+                SEND: begin
+                    en_tracking <= 1;
+                    case (object_color_detected)
+                        1: ssd_code <= 1;
+                        2: ssd_code <= 2;
+                        3: ssd_code <= 3;
+                    endcase
+                    object_color_detected <= object_color;
+                end
+                MATCH: begin
+                    case (object_color_detected)
+                        1: ssd_code <= 4;
+                        2: ssd_code <= 5;
+                        3: ssd_code <= 6;
+                    endcase
+                    en_tracking <= 0;
+                    en_buzz <= 1;
+                end
+                UTURN: begin
+                    ssd_code <= 8;
+                    en_uturn <= 1;
+                    en_buzz <= 0;
+                end
+                RETURN: begin
+                    en_tracking <= 1;
+                    en_uturn <= 0;
+                    ssd_code <= 9;
+                    object_color_detected <= 0;
+                    returning <= 1;
+                end
+                EOT:
+                    ssd_code <= 7;
+                    en_tracking <= 0;
+                    en_buzz <= 1;
+                end
 
 endmodule
